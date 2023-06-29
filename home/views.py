0066fd14ecd5fcad9649d370ba3d8ec5f2582395
cedriclemercier <info@cedricdesigns.com.au>
2023-06-29 22:35:41 +0700

    fixed all, add countdown and styling and all that

--- home/views.py
+++ home/views.py
@@ -12,27 +12,41 @@ from django.contrib.auth.decorators import login_required
 
 from .forms import EssayForm
 
-from .prompts import task_achievement, coherence_and_cohesion, lexical_resource, grammatical_range_accuracy
+# from .prompts import task_achievement, coherence_and_cohesion, lexical_resource, grammatical_range_accuracy
 
 import json
 import os
-import openai
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-sample_writing_text = """One of the most debatable issues of the last century has been the extent to which international trade benefits or harms national economies. Many arguments have been made for and against free trade between nations. In this essay, I will discuss both views and state my own position""".
+sample_writing_text = """One of the most debatable issues of the last century has been the extent to which international trade benefits or harms national economies. Many arguments have been made for and against free trade between nations. In this essay, I will discuss both views and state my own position."""
 
 sample_text = "Social networking sites, for instance Facebook, are teach by some to have a detrimental effect on individual people as well as society and local communities. However, while I believe that such sites are mainly beneficial to the individual, I agree that they have had a damaging effect on local communities.  With regards to individuals, the impact that online social media has had on each individual person has clear advantages. Firstly, people from different countries are brought together through such sites as Facebook whereas before the development of technology and social networking sites, people rarely had the chance to meet or communicate with anyone outside of their immediate circle or community. Secondly, Facebook also has social groups which offer individuals a chance to meet and participate in discussions with people who share common interests. On the other hand, the effect that Facebook and other social networking sites have had on societies and local communities can only be seen as negative. Rather than individual people taking part in their local community, they are instead choosing to take more interest in people online. Consequently, the people within local communities are no longer forming close or supportive relationships. Furthermore, society as a whole is becoming increasingly disjointed and fragmented as people spend more time online with people they have never met face to face and who they are unlikely to ever meet in the future. To conclude, although social networking sites have brought individuals closer together, they have not had the same effect on society or local communities. Local communities should do more to try and involve local people in local activities  in order to promote the future of community life."
 
+def convert_seconds(seconds):
+    minutes = seconds // 60  # 60 seconds in a minute
+    remaining_seconds = seconds % 60
+
+    # Format the output
+    if minutes == 1:
+        minutes_str = "1 minute"
+    else:
+        minutes_str = f"{minutes} minutes"
+
+    if remaining_seconds == 1:
+        seconds_str = "1 second"
+    else:
+        seconds_str = f"{remaining_seconds} seconds"
+
+    return f"{minutes_str}, {seconds_str}"
 def put_tags(text, lexical_ranges=None, grammar_ranges=None):
     # 1. Step1: Put words under tags
     words = text.split()
     # Concat arrays
     
-    btn_classes = ['btn-warning', 'btn-danger']
+    btn_classes = ['btn-warning', 'theme-bg']
     error_class = ['lexical', 'grammar']
     
     all_ranges = [lexical_ranges, grammar_ranges]
+    second_offset = 0
     for i, curr_range in enumerate(all_ranges):
         if curr_range is None:
             continue
@@ -40,8 +54,10 @@ def put_tags(text, lexical_ranges=None, grammar_ranges=None):
             # temp fix
             # start -= 1
             # end -= 1
-            offset = (idx * 2) + (2 * i)
-            words[start+offset:end+offset] = [f'<button class=" {btn_classes[i]}" type="button" data-toggle="collapse" data-target="#{error_class[i]}{idx+1}" aria-expanded="false" aria-controls="lexical{idx+1}">'] + words[start+offset:end+offset] + [f'</button>']
+            if lexical_ranges: second_offset == i
+            offset = (idx * 2) + (2 * second_offset)
+            print(offset)
+            words[start+offset:end+offset] = [f'<button class=" {btn_classes[i]}" type="button" data-toggle="collapse" style="border:0; padding: 0;" data-target="#{error_class[i]}{idx+1}" aria-expanded="false" aria-controls="lexical{idx+1}"><span style="padding: 2px 3px" data-toggle="tooltip" title="({idx+1})"><span>'] + words[start+offset:end+offset] + [f'</button>']
         result = ' '.join(words)
     return result
     
@@ -66,25 +82,36 @@ def writing_page(request):
       }
     
     response = requests.post(settings.API_URL + '/api/scoring/', json=payload).json()
+    
+    print(response)
     # Lexical spans
-    lexical_spans = response['lexical']['spans'][0]
+    lexical_spans = None
+    if "spans" in response['lexical']:
+      lexical_spans = response['lexical']['spans'][0]
     grammar_spans = response['grammar_range']['spans']
+    
+    
+    # Scores
+    task_achievement_score = response['task_achievement']['score']
+    coherence_cohesion_score = response['coherence_cohesion']['score']
+    lexical = response['lexical']['score']
+    grammar_range_score = response['grammar_range']['score']
 
     user_answer_corrected = put_tags(sample_text, lexical_spans, grammar_spans)
     
     # Get feedback from ChatGPT API
-    question = payload["question_text"]
-    answer = payload["user_answer"]
+    # question = payload["question_text"]
+    # answer = payload["user_answer"]
 
-    feedback = {}
+    # feedback = {}
 
-    feedback['task_achievement'] = json.loads(task_achievement(question, answer))
-    feedback['coherence_and_cohesion'] = json.loads(coherence_and_cohesion(question, answer))
-    feedback['lexical_resource'] = json.loads(lexical_resource(question, answer))
-    feedback['grammatical_range_accuracy'] = json.loads(grammatical_range_accuracy(question, answer))
+    # feedback['task_achievement'] = json.loads(task_achievement(question, answer))
+    # feedback['coherence_and_cohesion'] = json.loads(coherence_and_cohesion(question, answer))
+    # feedback['lexical_resource'] = json.loads(lexical_resource(question, answer))
+    # feedback['grammatical_range_accuracy'] = json.loads(grammatical_range_accuracy(question, answer))
 
-    print("==============FEEDBACK DICT===============")
-    print(feedback)
+    # print("==============FEEDBACK DICT===============")
+    # print(feedback)
 
     # Dummy feedback
     # response = requests.post(settings.API_URL + '/api/scoring/', json=payload).json()
@@ -92,6 +119,8 @@ def writing_page(request):
     
     data = payload
     data['topic'] = payload['question_topic']
+    
+    numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
     
     context = {
     'segment': 'writing',
@@ -101,6 +130,8 @@ def writing_page(request):
     'scored': True,
     'results': response,
     'question': data,
+    'overall_score': response['overall_score'],
+    'overall_score_letters': numbers[round(response['overall_score'])],
     'percentages': {
       'grammar': int(response['grammar_range']['score'] * 10),
       'lexical': int(response['lexical']['score'] * 10),
@@ -127,11 +158,10 @@ def writing_page(request):
     return render(request, 'pages/writing.html')
   
   
-  
   data = requests.get(settings.API_URL + '/api/questions/').json()
   random_question = data[random.randint(1, len(data)-1)]
   random_question['question_topic'] = random_question['topic']
-  random_question['user_answer'] = 'Type here...'
+  random_question['user_answer'] = "Social networking sites, for instance Facebook, are teach by some to have a detrimental effect on individual people as well as society and local communities. However, while I believe that such sites are mainly beneficial to the individual, I agree that they have had a damaging effect on local communities.  With regards to individuals, the impact that online social media has had on each individual person has clear advantages. Firstly, people from different countries are brought together through such sites as Facebook whereas before the development of technology and social networking sites, people rarely had the chance to meet or communicate with anyone outside of their immediate circle or community. Secondly, Facebook also has social groups which offer individuals a chance to meet and participate in discussions with people who share common interests. On the other hand, the effect that Facebook and other social networking sites have had on societies and local communities can only be seen as negative. Rather than individual people taking part in their local community, they are instead choosing to take more interest in people online. Consequently, the people within local communities are no longer forming close or supportive relationships. Furthermore, society as a whole is becoming increasingly disjointed and fragmented as people spend more time online with people they have never met face to face and who they are unlikely to ever meet in the future. To conclude, although social networking sites have brought individuals closer together, they have not had the same effect on society or local communities. Local communities should do more to try and involve local people in local activities  in order to promote the future of community life."
   
   form = EssayForm(random_question)
   # form.fields['user_answer'].required = False
