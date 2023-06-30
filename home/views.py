55dfe3e9474afc87a998eb53f9f632d3e05c7b1d
anuj-guptaa <anujgupta@windowslive.com>
2023-06-30 17:51:54 +0700

    display correct user answer

--- home/views.py
+++ home/views.py
@@ -144,8 +144,9 @@ def writing_page(request):
   data = requests.get(settings.API_URL + '/api/questions/').json()
   random_question = data[random.randint(1, len(data)-1)]
   random_question['question_topic'] = random_question['topic']
-  random_question['user_answer'] = "Social networking sites, for instance Facebook, are teach by some to have a detrimental effect on individual people as well as society and local communities. However, while I believe that such sites are mainly beneficial to the individual, I agree that they have had a damaging effect on local communities.  With regards to individuals, the impact that online social media has had on each individual person has clear advantages. Firstly, people from different countries are brought together through such sites as Facebook whereas before the development of technology and social networking sites, people rarely had the chance to meet or communicate with anyone outside of their immediate circle or community. Secondly, Facebook also has social groups which offer individuals a chance to meet and participate in discussions with people who share common interests. On the other hand, the effect that Facebook and other social networking sites have had on societies and local communities can only be seen as negative. Rather than individual people taking part in their local community, they are instead choosing to take more interest in people online. Consequently, the people within local communities are no longer forming close or supportive relationships. Furthermore, society as a whole is becoming increasingly disjointed and fragmented as people spend more time online with people they have never met face to face and who they are unlikely to ever meet in the future. To conclude, although social networking sites have brought individuals closer together, they have not had the same effect on society or local communities. Local communities should do more to try and involve local people in local activities  in order to promote the future of community life."
-  
+  random_question['user_answer'] = random_question['user_answer']
+
+
   form = EssayForm(random_question)
   # form.fields['user_answer'].required = False
   
