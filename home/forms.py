66379f1f706375a57e08ea08ee2e422429697edc
anuj-guptaa <anujgupta@windowslive.com>
2023-06-30 17:34:03 +0700

    round corners

--- home/forms.py
+++ home/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class EssayForm(forms.Form):
-    user_answer = forms.CharField(label = '',widget=forms.Textarea(attrs={"rows":20, 'class':'form-control', 'style':'line-height: 2.0;', 'placeholder': 'Write your answer here...'}))
+    user_answer = forms.CharField(label = '',widget=forms.Textarea(attrs={"rows":20, 'class':'form-control', 'style':'line-height: 2.0; text-align: justify;', 'placeholder': 'Write your answer here...'}))
     question_type = forms.CharField(widget=forms.HiddenInput())
     question_text = forms.CharField(widget=forms.HiddenInput())
     question_topic = forms.CharField(widget=forms.HiddenInput())
