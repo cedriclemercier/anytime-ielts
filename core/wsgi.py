8d142e5324ba73cae20c0ea3fb115b941f23e81e
cedriclemercier <info@cedricdesigns.com.au>
2023-06-30 14:56:52 +0700

    Added vercel config

--- core/wsgi.py
+++ core/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
+WSGI_APPLICATION = 'vercel_app.wsgi.app'
+
 application = get_wsgi_application()
